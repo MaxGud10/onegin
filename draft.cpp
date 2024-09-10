61a1b72e4e7a4fc3f88450e53fffc6e861503cae
MaxGud10 <karatemaksim0110@gmail.com>
2024-09-10 19:57:33 +0300

    сделал версию через массив указетелй

--- draft.cpp
+++ draft.cpp
@@ -2,17 +2,22 @@
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
+#include <ctype.h>
 
-#define MAX_ROWS 15
-#define MAX_COLS 260
+const int MAX_ROWS = 15;
+const int MAX_COLS = 260;
 
-int sort_string(char text[][MAX_COLS]);
-int print(char text[][MAX_COLS]);
-void str_cpy_mx_kshtschTVR (char* dest, const char* src);
+//int sort_string(char* pointers[], char text[][MAX_COLS]);
+int sort_string(char* pointers[]);
+void str_cpy(char* dest, const char* src);
+int clean_string(const char* src, char* dest);
+//int print(char* pointers[], char text[][MAX_COLS]);
+int print(char* pointers[]);
 
-int main(void)
+int main (void)
 {
-    char text[MAX_ROWS][MAX_COLS] = {};
+    char  text    [MAX_ROWS][MAX_COLS] = {};
+    char* pointers[MAX_ROWS]           = {}; // массив указателей
 
     printf("<<<< ANIME file opening\n");
 
@@ -21,7 +26,6 @@ int main(void)
     if (file == NULL)
     {
         printf("File opening error: onegin.txt\n");
-
         return -1;
     }
 
@@ -30,50 +34,61 @@ int main(void)
     int row = 0;
     while (row < MAX_ROWS)
     {
-        printf ("%d...", row);
         if (fgets(text[row], MAX_COLS, file) == NULL)
-            break; 
+            break;
 
         // Удаление символа новой строки
-        int symbol_gad    = strcspn(text[row], "\n");  
-        char* address_gad = strchr (text[row], '\n');
+        int symbol_gad = strcspn(text[row], "\n");  
 
- //     printf("symbol_gad = %d\n", symbol_gad);
-        assert (symbol_gad == address_gad - text[row]);
-          
         if (text[row][symbol_gad] == '\n')
         {
             text[row][symbol_gad] = '\0';
         }
 
+        pointers[row] = text[row]; // Сохраняем индекс строки
         row++; 
-//  printf("row = %d\n", row);
     }
 
-    printf ("calling fclose...\n");
-
+    printf("calling fclose...\n");
     fclose(file);
+    printf("exited from fclose\n");
 
-    printf ("exited from fclose\n");
 
+    printf("<<<<< sort_string\n");
+    //sort_string(pointers, text); 
+    sort_string(pointers); // Передаем массив указателей и массив текст
+    printf("<<<<< sort_string\n");
 
-    printf("<<<<< sort_string");
-    sort_string(text);
-    printf(">>>>> sort_string");
-    
-    printf ("calling print():\n");
-    print(text);
-    /*for (int i = 0; i < count; i++) 
-    {
-        printf("%s\n", lines[i]);
-    }*/
+    printf("calling print():\n");
+    //print(pointers, text);
+    print(pointers); // Печатаем с использованием указателей
 
     return 0;
 }
 
-int sort_string(char text[][MAX_COLS])
+int clean_string(const char* src, char* dest) // const char* src - указатель на исходную строку которую нужно очистить( const  показывает что содержимое не будет изменяться)
 {
-    char ded[MAX_COLS] = "";
+    while(*src) // для итерации по каждому символу исходной строки 
+    {
+
+        int is_punct = ispunct((unsigned char)*src); // проверка на пунктуацию
+        int is_space = isspace((unsigned char)*src); // проверка на пробелы
+
+        if (is_punct == 0 && is_space == 0)
+        {
+            *dest++ = (char)tolower((unsigned char)*src);  // Записываем символ в нижнем регистре
+        }
+        src++;
+    }
+    return *dest = '\0';
+}
+
+//int sort_string(char* pointers[], char text[][MAX_COLS])
+int sort_string(char* pointers[])
+{
+    char ded    [MAX_COLS] = "";
+    char clean_a[MAX_COLS] = "";
+    char clean_b[MAX_COLS] = "";
 
     for (int i = 0; i < MAX_ROWS; i++)
     {
@@ -82,49 +97,54 @@ int sort_string(char text[][MAX_COLS])
         for (int j = 0; j < MAX_ROWS - 1 - i; j++)
         {
             printf("j = %d...\n", j);
-            assert(0 <= j   && j < MAX_ROWS);
-            assert(0 <= j+1 && j+1 < MAX_COLS);
+            assert(0 <= j     && j     < MAX_ROWS);
+            assert(0 <= j + 1 && j + 1 < MAX_ROWS);
 
-            printf ("<%s>\tvs <%s>...", text[j], text[j+1]);
+            printf("<%s> vs <%s>...\n", pointers[j], pointers[j + 1]);
 
-            if (/*strlen(text[i]) > 0 && strlen(text[j]) > 0 &&*/ strcmp(text[j], text[j+1]) > 0) 
+            // Очищаем строки от знаков препинания и пробелов
+            clean_string(pointers[j],     clean_a);
+            clean_string(pointers[j + 1], clean_b);
+
+            if (strcasecmp(clean_a, clean_b) > 0) 
             {
-                printf ("Swapping...\n");
+                printf("Swapping...\n");
 
-                str_cpy_mx_kshtschTVR (ded, text[j]);
-                str_cpy_mx_kshtschTVR (     text[j], text[j+1]);
-                str_cpy_mx_kshtschTVR (              text[j+1], ded);
-    
-                printf ("<%s>\tvs <%s>: SWAPPED\n", text[j], text[j+1]);
+                str_cpy(ded, pointers[j]);
+                str_cpy(     pointers[j], pointers[j + 1]);
+                str_cpy(                  pointers[j + 1], ded);
+
+                printf("<%s> vs <%s>: SWAPPED\n", pointers[j], pointers[j + 1]);
             }
         }
-        
+
         printf("END PASS %d\n", i);
     }
     return 0;
 }
 
-void str_cpy_mx_kshtschTVR (char* dest, const char* src)
+void str_cpy(char* dest, const char* src)
 {
-    assert (src);
-    assert (dest);
+    assert(src);
+    assert(dest);
 
     for (int i = 0; i < MAX_COLS; i++)
     {
         dest[i] = src[i];
-    } 
+    }
 }
 
-int print(char text[][MAX_COLS])
+//int print(char* pointers[], char text[][MAX_COLS])
+int print(char* pointers[])
 {
-    printf ("THE TEXT:\n\n");
+    printf("THE TEXT:\n\n");
 
     for (int i = 0; i < MAX_ROWS; i++) 
     {
-        printf("<<< i = %02d: ", i);
-//        if (strlen(text[i]) > 0) // Печатаем только инициализированные строки
+        if (pointers[i] != NULL) // Проверка, чтобы избежать вывода NULL
         {
-            printf("<%s>\n", text[i]);
+            printf("<<< i = %02d: ", i);
+            printf("<%s>\n", pointers[i]);
         }
     }
     return 0;
